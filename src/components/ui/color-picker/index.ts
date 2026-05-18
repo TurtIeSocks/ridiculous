79b91fd7543c647be99c359dde8979cb578d840d
Rin <58572875+TurtIeSocks@users.noreply.github.com>
2026-05-17 20:53:49 -0400

    Add color-picker barrel index

---
+++ src/components/ui/color-picker/index.ts
@@ -0,0 +1,24 @@
+export { ColorPicker } from "./color-picker"
+export type { ColorPickerProps } from "./color-picker"
+
+export { color } from "./color-picker.types"
+
+export type {
+  ColorLiteral,
+  ColorMode,
+  ColorString,
+  ColorStringMap,
+  HexLiteral,
+  HexString,
+  HSLLiteral,
+  HslString,
+  HWBLiteral,
+  HwbString,
+  OklabLiteral,
+  OklabString,
+  OKLCHLiteral,
+  OklchString,
+  RGBALiteral,
+  RGBLiteral,
+  RgbString,
+} from "./color-picker.types"
